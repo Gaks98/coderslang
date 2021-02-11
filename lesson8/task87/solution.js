22d89e5d6dd65dfb82738de9120df0c1a4fd2714
Gaks98 <yvonnegax98@gmail.com>
2021-02-11 13:43:39 +0300

    structural folders

---
+++ lesson8/task87/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+const daysOfWeek = 7;
+const monthsInYear = 12;
+const boss = 'Yvonne';
+const port = 3000;
+const name = 'JJ';
