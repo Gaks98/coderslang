9fd2d4587bc4ae41e3f3a6e4df7bd5f7c355a703
Gaks98 <yvonnegax98@gmail.com>
2021-02-11 14:21:36 +0300

    deleted

--- lesson9/task90/solution.js
+++
@@ -1,9 +0,0 @@
-/**
- * Import constants from the constants.js file and log them to the console using console.log
- *
- * Each console.log should log a separate constant to the screen
- * */
-import {MAX_THREADS,API_URL} from './constants'
-
-console.log(MAX_THREADS);
-console.log(API_URL);
--- lesson9/task94/constants.js
+++
@@ -1,4 +0,0 @@
-export const heyThere = 'Hey there!';
-export const seeYou = 'See you!';
-
-
