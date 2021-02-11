cae7480d8be9d69048641aed584b87a72fd2e2e0
Gaks98 <yvonnegax98@gmail.com>
2021-02-11 14:28:33 +0300

    structural folders

--- task103/helper.js
+++
@@ -1,5 +0,0 @@
-const PI = 3.14159265359;
-
-export const circleLength = (radius) => {
-  return PI * 2 * radius;
-}
--- task92/helper.js
+++
@@ -1,6 +0,0 @@
-import readlineSync  from 'readline-sync';
-export const askNameAndGreet = () => {
-  const name = readlineSync.question('Please enter your name: ');
-
-  console.log(`Hello, ${name}!`);
-}
