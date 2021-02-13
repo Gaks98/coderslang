451451aa1aef2e6188b9af6028880cc7ed627952
Gaks98 <yvonnegax98@gmail.com>
2021-02-13 17:25:50 +0300

    lesson 11

---
+++ task111/helper.js
@@ -0,0 +1,6 @@
+export const user = {
+  age: '100',
+  name: 'John',
+  surname: 'Michael',
+  address: 'Giant street'
+};
---
+++ task115/helper.js
@@ -0,0 +1,11 @@
+export const objects = [
+  {
+    type: 'cat'
+  },
+   {
+    type: 'dog'
+  },
+  {
+    type: 'goat'
+  },
+];
